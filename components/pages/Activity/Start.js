b7a4ec37f17cc17878fd007b768a2e5aa2890b68
Anton Shpylenia <shpan93@gmail.com>
2017-09-24 04:35:31 +0300

    fix navigation

--- components/pages/Activity/Start.js
+++ components/pages/Activity/Start.js
@@ -1,33 +1,13 @@
 import React from 'react';
 import * as Ons from 'react-onsenui';
-import { connect } from 'react-redux';
 import ActivityStartForm from './ActivityStartForm';
-import { navigate } from '../../../redux/application/actions';
 
-class LoginPage extends React.PureComponent {
+class Start extends React.PureComponent {
   static propTypes = {};
-
-  renderToolbar() {
-    return (
-      <Ons.Toolbar>
-        <div className='center'>Splitter+Navigator</div>
-        <div className='right'>
-          <Ons.ToolbarButton onClick={this.props.showMenu.bind(this)}>
-            <Ons.Icon icon='ion-navicon, material:md-menu' />
-          </Ons.ToolbarButton>
-        </div>
-      </Ons.Toolbar>
-    );
-  }
-
-  navigateToRegister(e) {
-    e.preventDefault();
-    this.props.navigate('register');
-  }
 
   render() {
     return (
-      <Ons.Page renderToolbar={::this.renderToolbar} key="login">
+      <Ons.Page key="ac-start">
         <div className="activity-page page-wrapper wrapper align-height">
           <h1>Start activity</h1>
           <ActivityStartForm {...this.props} />
@@ -37,4 +17,4 @@ class LoginPage extends React.PureComponent {
   }
 }
 
-export default connect(null, { navigate })(LoginPage);
+export default Start;
